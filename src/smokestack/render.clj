017c73cb58385c708491695cf02d2222145add98
Timothy Pratley <timothypratley@gmail.com>
2014-03-10 21:17:28 -0700

    fixing CSS errors

--- src/smokestack/render.clj
+++ src/smokestack/render.clj
@@ -3,30 +3,46 @@
             [hiccup.core :refer [html]]
             [garden.core :refer [css]]))
 
-(def style (css [:html {:background "black"
-                        :color "white"
-                        :font-size "85%"
-                        :font-family "Consolas, \"Liberation Mono\", Courier, monospace"}]
-                ;TODO: is this the correct syntax for .msg h1, .msg h3
-                [:.msg :h1 :.msg :h3 {:font-weight 800
-                                      :font-size "1em"
-                                      :line-height "40px"
-                                      :padding "10px 0 10px 20px"
-                                      :background "green"}]
-                [:.msg :h3 {:font-weight 600
-                            :font-size "0.8em"
-                            :line-height "30px"
-                            :margin-top "10px"}]
-                [:code {:margin "20px"}]
-                [:.error-line {:background "#a60000"
-                               :font-weight 800
-                               :margin "10px 0"
-                               :font-size "120%"
-                               :padding "10px 0"}]
-                [:ul {:list-style-type "none"}]
-                [:.ln {:margin-right "10px"}]
-                [:.stack {:text-align "right"
-                          :padding-right "30px"}]))
+(def style
+  (css [:html {:background "black"
+               :color "white"
+               :font-size "85%"
+               :font-family "Consolas, \"Liberation Mono\", Courier, monospace"}]
+
+       [:.msg
+        [:h1 :h3
+         {:font-weight 800
+          :font-size "1em"
+          :line-height "40px"
+          :padding [["10px" "0" "10px" "20px"]]
+          :background "green"}]]
+
+       [:.msg
+        [ :h3
+         {:font-weight 600
+          :font-size "0.8em"
+          :line-height "30px"
+          :margin-top "10px"}]]
+
+       [:code
+        {:margin "20px"}]
+
+       [:.error-line
+        {:background "#a60000"
+         :font-weight 800
+         :margin "10px 0"
+         :font-size "120%"
+         :padding "10px 0"}]
+
+       [:ul
+        {:list-style-type "none"}]
+
+       [:.ln
+        {:margin-right "10px"}]
+
+       [:.stack
+        {:text-align "right"
+         :padding-right "30px"}]))
 
 (defn html-code
   "Renders the block of code around a line"
